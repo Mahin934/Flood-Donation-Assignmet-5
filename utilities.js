7941ed0c6c4977b6d5d9958d6ba809d56301da67
Mahin934 <mahinmia934@gmail.com>
2024-09-24 11:16:22 +0600

    History function and Modal added

--- utilities.js
+++ utilities.js
@@ -4,11 +4,11 @@ function getInputValue(id) {
     const inputValue = document.getElementById(id).value;
     const inputValueNumber = parseFloat(inputValue);
     return inputValueNumber;
-}
+};
 
 // for Texts to Number Convert
 function getInnerText(id) {
     const inputText = document.getElementById(id).innerText;
     const inputTextNumber = parseFloat(inputText);
     return inputTextNumber;
-}
+};
